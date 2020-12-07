b8374089df09d12510400ae33ec8059a37f22087
Marco <m_s_f_@hotmail.com>
2020-12-07 18:05:56 +0000

    Faz todos os pedidos com sucesso
    Busca os todos dados relativos às diversas cidades.
    Prox passo: carregar para a LV e tratar da atividade de detalhe(secundária)

--- app/src/main/java/com/example/weatherapp/AsyncTasks/AsyncTaskTempo.java
+++ app/src/main/java/com/example/weatherapp/AsyncTasks/AsyncTaskTempo.java
@@ -2,10 +2,13 @@ package com.example.weatherapp.AsyncTasks;
 
 import android.os.AsyncTask;
 import android.util.Log;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.example.weatherapp.Objetos.DadosMeteo;
 import com.example.weatherapp.R;
 
+import org.apache.commons.lang3.StringUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -17,7 +20,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 /**
- * Asynctask que permite fazer o download de dados json relativos à meteorologia de
+ * Asynctask que permite fazer o download de dados JSON relativos à meteorologia de
  * várias cidades, incluíndo a cidade corrente do utilizador
  *
  * @author Marco Seiça Fidalgo
@@ -26,49 +29,125 @@ import java.net.URL;
  * @copyright Projeto open-source
  *
  */
-public class AsyncTaskTempo extends AsyncTask<String, Void, String> {
+public class AsyncTaskTempo extends AsyncTask<String, Void, Object> {
+    private static final String API_KEY = "6b09297fd3edfcc07d3df5c3fb286350";
+    private static final String URL_OPENWEATHER = "https://api.openweathermap.org/data/2.5/weather?q=<CIDADE>&appid=<API_KEY>&units=metric";
 
-    private TextView texto;
+    //private static final String URL_OPENWEATHER_IDS = "https://api.openweathermap.org/data/2.5/group?id=<CIDADE>&appid=<API_KEY>&units=metric";
+    //private static final String LISTA_CIDADES = "{2267057,3117735,2968815,2009543,2618425,3169070,2643743,2964574,3067696,2761369}";
+
+    private TextView tvCidade = null;
+    private ListView lvCidades = null;
     private String cidade;
+    private String[] listaCidades;
 
 
-    public AsyncTaskTempo(TextView texto) {
-        this.texto = texto;
+    public AsyncTaskTempo(TextView tvCidade, String cidade) {
+        this.tvCidade = tvCidade;
+        this.cidade = cidade;
+    }
+
+    public AsyncTaskTempo(ListView lvCidades, String[] listaCidades) {
+        this.lvCidades = lvCidades;
+        this.listaCidades = listaCidades;
     }
 
 
     @Override
-    protected String doInBackground(String... params) {
+    protected Object doInBackground(String... params) {
         String resultado = "(erro)";
+
         try {
-            resultado = getDados(params[0]);
-            this.cidade = params[1];
+            if(this.tvCidade != null) {
+                resultado = getDadosCidade( trataURL(URL_OPENWEATHER,cidade) );
+                return trataResultado(resultado);
+            }
+            else if(this.lvCidades != null){
+                DadosMeteo[] dadosCidades = new DadosMeteo[listaCidades.length];
+                for (int i = 0; i < listaCidades.length; i++) {
+                    resultado = getDadosCidade( trataURL(URL_OPENWEATHER,listaCidades[i]));
+                    dadosCidades[i] = trataResultado(resultado);
+                }
+                return dadosCidades;
+            }
         } catch (Exception e) {
             Log.d("testee","[ERRO] doInBackground AsyncTaskTempo - Erro: "+e);
         }
         return resultado;
     }
     @Override
-    protected void onPostExecute(String resultado) {
-        //texto.setText(resultado);
-        try {
-            JSONObject obj = new JSONObject(resultado);
-           // JSONArray lista = obj.getJSONArray("list");//Usado em Arrays [ ]
-            //JSONObject obj1 = lista.getJSONObject(2);
-            JSONObject main = obj.getJSONObject("main");
-            double temperatura = main.getDouble("temp");
+    protected void onPostExecute(Object objDados) {
+        //SE RECEBER um DADOSMETEO é singular, se receber um array de dadosmeteo é a lista de cidades
 
-            JSONObject o1 = new JSONObject();
-            texto.setText(cidade+"\n"+temperatura+"º");
+        //Cidade Corrente
+        if (objDados instanceof DadosMeteo) {
+            DadosMeteo dados = (DadosMeteo)objDados;
+            tvCidade.setText(cidade + "\n" + dados.getTemperatura() + "º");
+
         }
-        catch (JSONException e) {
-            texto.setText("Erro a ler informacao JSON");
-            Log.d("testee","[ERRO] onPostExecute AsyncTaskTempo - Erro: "+e);
+        //Lista de cidades
+        else if(objDados instanceof DadosMeteo[]){
+            DadosMeteo[] dados = (DadosMeteo[])objDados;
+            //Corre todos os elementos, os que estiverem a null não vão para o adapter
+            if(dados.length > 0) {
+                //TODO: enviar os dados para a ListView
+            }
+            else {
+                //nao encontrou nenhuma das cidades pretendidas
+                tvCidade.setText("Não foi possível encontrar as cidades pretendidas");
+            }
+        }
+        else{
+            //Caso receba um null
+            tvCidade.setText("Não foi possível encontrar a cidade corrente");
         }
 
 
     }
-    String getDados(String endereco) {
+
+    //## Funções auxiliares
+
+    /** Trata resultado do pedido devolvendo um objeto 'DadosMeteo' */
+    public DadosMeteo trataResultado(String resultado){
+        try {
+            JSONObject obj = new JSONObject((String) resultado);
+            // JSONArray lista = obj.getJSONArray("list");//Usado em Arrays [ ]
+            //JSONObject obj1 = lista.getJSONObject(2);
+
+            JSONArray  weather = obj.getJSONArray("weather");
+            JSONObject  main = obj.getJSONObject("main"),
+                        wind = obj.getJSONObject("wind");
+            double temperatura = main.getDouble("temp");
+
+            JSONObject o1 = new JSONObject();
+
+            String a = (weather.getJSONObject(0)).getString("main");
+
+            return new DadosMeteo(
+                    (weather.getJSONObject(0)).getString("icon"),       // Ícone da condi. atmosférica
+                    obj.getString("name"),                                     // Nome cidade
+                    (weather.getJSONObject(0)).getString("main"),       // Condição
+                    (weather.getJSONObject(0)).getString("description"),// Descrição da condição
+                    main.getDouble("temp"),                                   // Temperatura atual
+                    main.getDouble("feels_like"),                             // Sensação térmica
+                    main.getDouble("temp_min"),                               // Temperatura mínima
+                    main.getDouble("temp_max"),                               // Temepratura máxima
+                    main.getInt("pressure"),                                  // Pressão atm.
+                    main.getInt("humidity"),                                  // Humidade relativa
+                    wind.getDouble("speed"));                                 // Velocidade vento
+        }
+        catch(JSONException je){
+            je.printStackTrace();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    /** Faz o pedido ao API enviando o URL tratado e recebendo os dados JSON como String */
+    String getDadosCidade(String endereco) {
         StringBuilder resp = new StringBuilder();
         try {
             URL url = new URL(endereco);
@@ -94,4 +173,13 @@ public class AsyncTaskTempo extends AsyncTask<String, Void, String> {
         }
         return resp.toString();
     }
+
+    /** Modifica a URL para aceder à API consoante a cidade/API_KEY*/
+    public String trataURL(String urlBase, String cidades){
+        String url;
+        url = StringUtils.replace(urlBase,"<CIDADE>",cidades);
+        url = StringUtils.replace(url,"<API_KEY>",API_KEY);
+        return url;
+    }
+
 }
--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.weatherapp;
+ package com.example.weatherapp;
 /**
  * Atividade principal de introdução à aplicação. Mostra Um resumo da meteorologia
  * da cidade corrente do utilizador.
@@ -11,30 +11,40 @@ package com.example.weatherapp;
  *
  */
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
+ import android.Manifest;
+ import android.content.Context;
+ import android.content.DialogInterface;
+ import android.content.Intent;
+ import android.content.pm.PackageManager;
+ import android.location.Address;
+ import android.location.Geocoder;
+ import android.location.LocationManager;
+ import android.net.ConnectivityManager;
+ import android.net.NetworkInfo;
+ import android.os.Bundle;
+ import android.provider.Settings;
+ import android.util.Log;
+ import android.widget.ListView;
+ import android.widget.TextView;
+ import android.widget.Toast;
 
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.os.Bundle;
-import android.util.Log;
-import android.widget.TextView;
+ import androidx.appcompat.app.AlertDialog;
+ import androidx.appcompat.app.AppCompatActivity;
+ import androidx.core.app.ActivityCompat;
 
+ import com.example.weatherapp.AsyncTasks.AsyncTaskTempo;
+ import com.google.android.gms.location.FusedLocationProviderClient;
+ import com.google.android.gms.location.LocationServices;
 
-import com.example.weatherapp.AsyncTasks.AsyncTaskTempo;
+ import org.apache.commons.lang3.StringUtils;
 
-import org.apache.commons.lang3.StringUtils;
+ import java.util.List;
+ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String API_KEY = "6b09297fd3edfcc07d3df5c3fb286350";
-    private static final String URL_OPENWEATHER = "https://api.openweathermap.org/data/2.5/weather?q=<CIDADE>&appid=<API_KEY>&units=metric";
+    private static final String[] LISTA_CIDADES = {"Lisboa", "Madrid", "Paris", "Berlim", "Copenhaga", "Roma", "Londres", "Dublin", "Praga", "Viena"};
 
+    boolean gpsLigado = false;
 
     private String cidade = "Lisboa";   //Por defeito fica Lisboa
 
@@ -44,57 +54,157 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView tvCidadeCorr = findViewById(R.id.main_cidadeCorrente);
+        pedePermissoes();
+        verificaSeTemGPSLigado();
 
 
-    //Lança asynctask para a Cidade Atual
-        ConnectivityManager connMgr = (ConnectivityManager)
-                getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
 
-        if (networkInfo != null && networkInfo.isConnected()) {
-            AsyncTaskTempo asyncTaskTempo = new AsyncTaskTempo(tvCidadeCorr);
-            asyncTaskTempo.execute(trataURL(cidade), cidade);
-        } else {
-            tvCidadeCorr.setText(R.string.sem_internet);
+
+
+        //Lança asynctask para a Cidade Atual
+        if(gpsLigado){
+            /*NOTA: Como existe a impossibilidade de pesquisar por vários nomes de cidade mas apenas por IDs,
+                    foi tomada a decisão de fazer 2 pedidos distintos.
+                    1 para a cidade corrente e outro para as restantes cidades.
+
+                    A solução alternativa com apenas um pedido, seria procurar pelo ID da cidade corrente
+                    no fich. JSON disponibilizado no site da API e juntar aos IDS das restantes cidades*/
+            buscaCidadeCorr();
+            buscaCidadesRestantes();
         }
     }
 
     /** Busca cidade corrente */
-    public void buscaCidadeCorr(){
+    public void buscaCidadeCorr() {
+        try {
+            FusedLocationProviderClient mLocationProvider = LocationServices.getFusedLocationProviderClient(this);
+            mLocationProvider.getLastLocation().addOnSuccessListener(this, location -> {
+                /*NOTA: Location pode ficar a null nestas situações:
+                        https://droidbyme.medium.com/get-current-location-using-fusedlocationproviderclient-in-android-cb7ebf5ab88e
+                        Não vou tratar estas situações agora
+                */
+                if (location != null) {
+                    double latitude = location.getLatitude();
+                    double longitude = location.getLongitude();
+                    try {
+                        Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+                        List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
 
-    //Localização
-        LocationManager mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+                        Log.d("testee", "CIDADE: " + addresses.get(0).getLocality());
+                        cidade = addresses.get(0).getLocality();
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
+                        ConnectivityManager connMgr = (ConnectivityManager)
+                                getSystemService(Context.CONNECTIVITY_SERVICE);
+                        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+                        TextView tvCidadeCorr = findViewById(R.id.main_cidadeCorrente);
+                        if (networkInfo != null && networkInfo.isConnected()) {
+                            AsyncTaskTempo asyncTaskTempo = new AsyncTaskTempo(tvCidadeCorr,cidade);
+                            asyncTaskTempo.execute();
+                        } else
+                            tvCidadeCorr.setText(R.string.sem_internet);
+                    }
+                    catch(Exception e){
+                        e.printStackTrace();
+                    }
+                }
+            }).addOnFailureListener(e -> {
+                Log.d("testee","addOnFailureListener");
+            });
+        }
+        catch(SecurityException e){e.printStackTrace();}
+    }
+
+    /** Busca restantes cidades */
+    public void buscaCidadesRestantes() {
+        try {
+            ConnectivityManager connMgr = (ConnectivityManager)
+                    getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+            ListView lvCidades = findViewById(R.id.main_listView);
+            if (networkInfo != null && networkInfo.isConnected()) {
+                AsyncTaskTempo asyncTaskTempo = new AsyncTaskTempo(lvCidades,LISTA_CIDADES);
+                asyncTaskTempo.execute();
+            }
+            else
+                ((TextView)findViewById(R.id.main_cidadeCorrente)).setText(R.string.sem_internet);
+        }
+        catch(SecurityException e){e.printStackTrace();}
+    }
+
+    /**
+     * Aux: https://www.youtube.com/watch?v=TnYXQHvuPIw&ab_channel=codestance */
+    private void verificaSeTemGPSLigado(){
+
+        boolean estadoInternet = false,
+                estadoGPS = false;
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        try {
+            estadoGPS = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);//High accuracy
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+
+        try {
+            estadoInternet = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        }
+        catch(Exception e){e.printStackTrace();}
+
+        if(!estadoGPS && !estadoInternet){
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("Localização desligada")
+                    .setCancelable(false)
+                    .setPositiveButton("Ligar", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            //redireciona o user para o painel de localização do dispositivo
+                            startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                        }
+                    })
+                    .setNegativeButton("Cancelar",null)
+                    .show();
+        }
+        else{
+            gpsLigado = true;
+        }
+
+
+    }
+
+    /** Pede Permissões de Localização */
+    private void pedePermissoes(){
+        //Permissões de localização em tempo real
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    1);
             return;
         }
-        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10,
-                1, mLocationListener);
 
     }
 
-    /** Modifica a URL para aceder à API consoante a cidade/API_KEY*/
-    public String trataURL(String cidade){
-        String url;
-        url = StringUtils.replace(URL_OPENWEATHER,"<CIDADE>",cidade);
-        url = StringUtils.replace(url,"<API_KEY>",API_KEY);
-        return url;
-    }
-
-    /** TODO: Está certo? Sempre que é recebido um update de posição, corre esta função*/
-    private final LocationListener mLocationListener = new LocationListener() {
-        @Override
-        public void onLocationChanged(final Location location) {
-            Log.d("testee","Loca NOVA");
-
+    /** Trata os pedidos de permissões em runtime */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,int[] grantResults){
+        switch (requestCode){
+            case 1:
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //Quando dá permissões de localização pela 1ª vez, busca a localização de seguida
+                    buscaCidadeCorr();
+                    buscaCidadesRestantes();
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "#############Permission denied to read your External storage", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            default:
+                break;
         }
-    };
+
+    }
 }
